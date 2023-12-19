877ad8ddfecb9727fedcb9ad4736b03aef0e5f34
Antonia Nascimento <antonianascimento2309@gmail.com>
2023-12-19 20:57:04 +0000

    done

--- src/pages/AllBeersPage.jsx
+++ src/pages/AllBeersPage.jsx
@@ -1,152 +1,68 @@
-import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import Search from '../components/Search';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
 
-function AddBeerPage() {
-    const navigate = useNavigate();
+function AllBeersPage() {
+    const [beers, setBeers] = useState([]);
 
-    const [name, setName] = useState('');
-    const [tagline, setTagline] = useState('');
-    const [description, setDescription] = useState('');
-    const [imageUrl, setImageUrl] = useState('');
-    const [firstBrewed, setFirstBrewed] = useState('');
-    const [brewersTips, setBrewersTips] = useState('');
-    const [attenuationLevel, setAttenuationLevel] = useState(0);
-    const [contributedBy, setContributedBy] = useState('');
-
-    const handleName = (e) => setName(e.target.value);
-    const handleTagline = (e) => setTagline(e.target.value);
-    const handleDescription = (e) => setDescription(e.target.value);
-    const handleImageUrl = (e) => setImageUrl(e.target.value);
-    const handleFirstBrewed = (e) => setFirstBrewed(e.target.value);
-    const handleBrewersTips = (e) => setBrewersTips(e.target.value);
-    const handleAttenuationLevel = (e) => setAttenuationLevel(e.target.value);
-    const handleContributedBy = (e) => setContributedBy(e.target.value);
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        try {
-            const response = await axios.post(
-                'https://ih-beers-api2.herokuapp.com/beers/new',
-                {
-                    name,
-                    tagline,
-                    description,
-                    image_url: imageUrl,
-                    first_brewed: firstBrewed,
-                    brewers_tips: brewersTips,
-                    attenuation_level: Number(attenuationLevel),
-                    contributed_by: contributedBy,
-                }
-            );
-
-            if (response.status === 200) {
-                console.log('New beer created successfully:', response.data);
-                navigate('/');
-            } else {
-                console.error('Error creating new beer:', response.data);
+    useEffect(() => {
+        const fetchBeers = async () => {
+            try {
+                const response = await axios.get(
+                    'https://ih-beers-api2.herokuapp.com/beers'
+                );
+                setBeers(response.data);
+            } catch (error) {
+                console.error('Error fetching beers:', error);
             }
-        } catch (error) {
-            console.error('Error creating new beer:', error);
-        }
-    };
+        };
+
+        fetchBeers();
+    }, []);
 
     return (
         <>
-            <div className='d-inline-flex flex-column w-100 p-4'>
-                <form onSubmit={handleSubmit}>
-                    <label>Name</label>
-                    <input
-                        className='form-control mb-4'
-                        type='text'
-                        name='name'
-                        placeholder='Beer Name'
-                        value={name}
-                        onChange={handleName}
-                    />
-                    <label>Tagline</label>
-                    <input
-                        className='form-control mb-4'
-                        type='text'
-                        name='tagline'
-                        placeholder='Beer Tagline'
-                        value={tagline}
-                        onChange={handleTagline}
-                    />
-                    <label className='form-label'>Description</label>
-                    <textarea
-                        className='form-control mb-4'
-                        type='text'
-                        name='description'
-                        placeholder='Description'
-                        rows='3'
-                        value={description}
-                        onChange={handleDescription}
-                    ></textarea>
-                    <label>Image</label>
-                    <input
-                        className='form-control mb-4'
-                        type='text'
-                        name='imageUrl'
-                        placeholder='Image URL'
-                        value={imageUrl}
-                        onChange={handleImageUrl}
-                    />
-                    <label>First Brewed</label>
-                    <input
-                        className='form-control mb-4'
-                        type='text'
-                        name='firstBrewed'
-                        placeholder='Date - MM/YYYY'
-                        value={firstBrewed}
-                        onChange={handleFirstBrewed}
-                    />
-                    <label>Brewer Tips</label>
-                    <input
-                        className='form-control mb-4'
-                        type='text'
-                        name='brewersTips'
-                        placeholder='...'
-                        value={brewersTips}
-                        onChange={handleBrewersTips}
-                    />
-                    <label>Attenuation Level</label>
-                    <div className='input-group mb-2'>
-                        <div className='input-group-prepend'>
-                            <span
-                                className='input-group-text'
-                                id='basic-addon1'
-                            >
-                                %
-                            </span>
-                        </div>
-                        <input
-                            className='form-control mb-4'
-                            type='number'
-                            name='attenuationLevel'
-                            value={attenuationLevel}
-                            onChange={handleAttenuationLevel}
-                            min={0}
-                            max={100}
-                        />
-                    </div>
-                    <label>Contributed By</label>
-                    <input
-                        className='form-control mb-4'
-                        type='text'
-                        name='contributedBy'
-                        placeholder='Contributed by'
-                        value={contributedBy}
-                        onChange={handleContributedBy}
-                    />
-                    <button type='submit' className='btn btn-primary btn-round'>
-                        Add Beer
-                    </button>
-                </form>
+            <Search />
+
+            <div className='d-inline-flex flex-wrap justify-content-center align-items-center w-100 p-4'>
+                {beers &&
+                    beers.map((beer, i) => {
+                        return (
+                            <div key={i}>
+                                <Link to={'/beers/' + beer._id}>
+                                    <div
+                                        className='card m-2 p-2 text-center'
+                                        style={{
+                                            width: '24rem',
+                                            height: '18rem',
+                                        }}
+                                    >
+                                        <div className='card-body'>
+                                            <img
+                                                src={beer.image_url}
+                                                style={{ height: '6rem' }}
+                                                alt={'image of' + beer.name}
+                                            />
+                                            <h5 className='card-title text-truncate mt-2'>
+                                                {beer.name}
+                                            </h5>
+                                            <h6 className='card-subtitle mb-3 text-muted'>
+                                                <em>{beer.tagline}</em>
+                                            </h6>
+                                            <p className='card-text'>
+                                                Created by:{' '}
+                                                {beer.contributed_by}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </Link>
+                            </div>
+                        );
+                    })}
             </div>
         </>
     );
 }
 
-export default AddBeerPage;
+export default AllBeersPage;
